d725896ae6ac2e9e0426ed0202ed82e402542da9
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-17 16:35:01 +0200

    update

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Comment } from "../Comment/Comment";
 import { Grid } from "../Grid/Grid";
-import { comments } from "../../helpers/comments";
+// import { comments } from "../../helpers/comments";
 import { selectorFilters } from "../../redux/filterSlice";
 import { useSelector } from "react-redux";
+import { useGetCommentsQuery } from "../../redux/commentApi";
+import { Loader } from "../Loader/Loader";
 
 export const Comments = () => {
   const filter = useSelector(selectorFilters);
-
+  const { data: comments, isLoading, isError } = useGetCommentsQuery();
+  // console.log(data);
   const filteredComponents = () => {
     return comments.filter((comment) =>
       comment.content.toLowerCase().includes(filter.toLowerCase())
@@ -17,6 +20,8 @@ export const Comments = () => {
 
   return (
     <Grid>
+      {isLoading && <Loader />}
+      {isError && <p>Oops,error!</p>}
       {comments &&
         filteredComponents().map((comment) => (
           <Comment key={comment.id} {...comment} />
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import { BiMailSend } from 'react-icons/bi';
-import styles from './Form.module.css';
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { BiMailSend } from "react-icons/bi";
+import styles from "./Form.module.css";
+import { useAddCommentsMutation } from "../../redux/commentApi";
 
 export const Form = () => {
-  const [author, setAuthor] = useState('');
-  const [content, setContent] = useState('');
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+  const [addComments, { isLoading }] = useAddCommentsMutation();
 
   const onHandleChange = (e) => {
     const { name, value } = e.target;
-    console.log(name, value);
+    if (name === "name") {
+      setAuthor(value);
+    }
+    if (name === "text") {
+      setContent(value);
+    }
   };
+
+  console.log(author, content);
 
   const onHandleSubmit = (e) => {
     e.preventDefault();
-
-    setAuthor('');
-    setContent('');
+    addComments({
+      author,
+      content,
+    });
+    setAuthor("");
+    setContent("");
   };
 
   return (
@@ -25,8 +37,8 @@ export const Form = () => {
         <label className={styles.label}>
           <span className={styles.labelName}>Full name</span>
           <input
-            type='text'
-            name='name'
+            type="text"
+            name="name"
             className={styles.input}
             value={author}
             onChange={onHandleChange}
@@ -37,8 +49,8 @@ export const Form = () => {
           <span className={styles.labelName}>Your comment</span>
           <textarea
             className={styles.input}
-            name='text'
-            rows='5'
+            name="text"
+            rows="5"
             value={content}
             onChange={onHandleChange}
           ></textarea>
@@ -46,7 +58,7 @@ export const Form = () => {
 
         <button className={styles.formBtn}>
           <BiMailSend className={styles.icon} />
-          Send
+          {isLoading ? "Sending" : "Send"}
         </button>
       </form>
     </div>
